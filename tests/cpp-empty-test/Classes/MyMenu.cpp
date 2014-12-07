8086f8d58bcf1882d88510d4eecd93e7681c2cf7
minggo <ming@cocos2d-x.org>
2014-12-07 11:57:31 +0800

    show and hide blocks when 2 seconds hit

--- tests/cpp-empty-test/Classes/MyMenu.cpp
+++ tests/cpp-empty-test/Classes/MyMenu.cpp
@@ -7,6 +7,8 @@
 //
 
 #include "MyMenu.h"
+#include "HelloWorldScene.h"
+#include "MapLayer.h"
 
 using namespace cocos2d;
 
@@ -20,9 +22,14 @@ void MyMenu::onTouchEnded(cocos2d::Touch* touch, cocos2d::Event* event)
     int diff = (time.tv_sec * 1000 + time.tv_usec / 1000) - _duration;
     
     // duration is 2 sends, and error is less than 200 ms
-    if (abs(diff - 2000) < 200)
+    if (abs(diff - 2000) < 400)
     {
         log("hit 2 seconds");
+    
+        HelloWorld* parent = static_cast<HelloWorld*>(getParent());
+        int column = parent->_playerPosIndex.columnIdx;
+        int row = parent->_playerPosIndex.rowIdx;
+        showAroundPlayer(column, row);
     }
     else
     {
@@ -64,5 +71,44 @@ MyMenu::MyMenu()
     Director::getInstance()->getScheduler()->schedule(function, this, 5, false, "show label");
     
     _duration = 0;
+}
+
+void MyMenu::showAroundPlayer(int column, int row)
+{
+    
+    
+    auto showFunc = [this, column, row](){
+        auto upPos = PosIndex(row+1, column);
+        auto leftUpPos = PosIndex(row+1, column-1);
+        auto rightUpPos = PosIndex(row+1, column+1);
+        
+        auto leftPos = PosIndex(row, column-1);
+        auto rightPos = PosIndex(row, column+1);
+        
+        auto bottomPos = PosIndex(row -1, column);
+        auto bottomLeftPos = PosIndex(row-1, column-1);
+        auto bottomRightPos = PosIndex(row-1, column+1);
+        
+        HelloWorld* parent = static_cast<HelloWorld*>(getParent());
+        parent->_mapLayer->showBlock(upPos);
+        parent->_mapLayer->showBlock(leftUpPos);
+        parent->_mapLayer->showBlock(rightUpPos);
+        parent->_mapLayer->showBlock(leftPos);
+        parent->_mapLayer->showBlock(rightPos);
+        parent->_mapLayer->showBlock(bottomPos);
+        parent->_mapLayer->showBlock(bottomLeftPos);
+        parent->_mapLayer->showBlock(bottomRightPos);
+        
+    };
+    
+    auto hideFunc = [this]() {
+        HelloWorld* parent = static_cast<HelloWorld*>(getParent());
+        parent->_mapLayer->hideAllBlocks();
+    };
+    
+    auto show = CallFunc::create(showFunc);
+    auto hide = CallFunc::create(hideFunc);
+    auto sequence = Sequence::create(show, DelayTime::create(2), hide, nullptr);
+    runAction(sequence);
 }
 
